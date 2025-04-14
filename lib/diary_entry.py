c52f5b6dd1ccc30437c9f7ef21edf43c6744197b
Steve Cottle <stevocottle@gmail.com>
2025-04-14 11:32:04 +0100

    Committing setup files

---
+++ lib/diary_entry.py
@@ -0,0 +1,41 @@
+class DiaryEntry:
+    def __init__(self, title, contents):
+        # Parameters:
+        #   title: string
+        #   contents: string
+        pass
+
+    def format(self):
+        # Returns:
+        #   A formatted diary entry, for example:
+        #   "My Title: These are the contents"
+        pass
+
+    def count_words(self):
+        # Returns:
+        #   int: the number of words in the diary entry
+        pass
+
+    def reading_time(self, wpm):
+        # Parameters:
+        #   wpm: an integer representing the number of words the user can read 
+        #        per minute
+        # Returns:
+        #   int: an estimate of the reading time in minutes for the contents at
+        #        the given wpm.
+        pass
+
+    def reading_chunk(self, wpm, minutes):
+        # Parameters
+        #   wpm: an integer representing the number of words the user can read
+        #        per minute
+        #   minutes: an integer representing the number of minutes the user has
+        #            to read
+        # Returns:
+        #   string: a chunk of the contents that the user could read in the
+        #           given number of minutes
+        #
+        # If called again, `reading_chunk` should return the next chunk,
+        # skipping what has already been read, until the contents is fully read.
+        # The next call after that should restart from the beginning.
+        pass
