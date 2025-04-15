72dde049f67fffd3b9e1d70eddc21f58b09cba40
Steve Cottle <stevocottle@gmail.com>
2025-04-15 08:39:34 +0100

    grammar_stats & test_grammar_stats WIP

--- lib/grammar_stats.py
+++ lib/grammar_stats.py
@@ -1,6 +1,9 @@
+
+
+
 class GrammarStats:
     def __init__(self):
-        pass
+        self.good_checks_exist = False #<-- This is a memory-box for if good checks exist
 
     def check(self, text):
         # Parameters:
@@ -8,10 +11,25 @@ class GrammarStats:
         # Returns:
         #   bool: true if the text begins with a capital letter and ends with a
         #         sentence-ending punctuation mark, false otherwise
-        pass
+        if (text[0].isupper()) and (text.endswith((".", ":", "?", "!"))): #<-- If the string matches the requirements ...
+            self.good_checks_exist = True # ... change the memory box to True ...
+            return True # ... and return True
+        else:
+            return False
 
     def percentage_good(self):
-        # Returns:
+        # Returns True:
         #   int: the percentage of texts checked so far that passed the check
         #        defined in the `check` method. The number 55 represents 55%.
-        pass
+
+        if self.good_checks_exist == True: # <--- If the memory box is set to true ...
+            return 100 # ... return 100 ...
+        else: # ... or else ...
+            return 0 # ... return 0
+
+
+
+instance_one = GrammarStats()
+print(f"check: {instance_one.check('Pigasdfdodsygt.')}")
+print(f"percentage good: {instance_one.percentage_good()}")
+print(f"check(text): {text}") #<--- Not working
--- tests/test_grammar_stats.py
+++ tests/test_grammar_stats.py
@@ -1,1 +1,120 @@
 from lib.grammar_stats import *
+
+
+# !!! This test is invalid because we don't 
+# """
+# given an empty string
+# #check can return the empty string
+# """
+# 
+# def test_check_returns_an_empty_string():
+#     test_instance = GrammarStats()
+#     test_instance.check("")
+#     result = ""
+#     assert result == ""
+# 
+# 
+# unltimately want the method to return a string
+# """
+# given an multi word string
+# #check can return the given string
+# """
+# def test_check_can_return_multi_word_string():
+#     test_instance = GrammarStats()
+#     result = test_instance.check("test string")
+#     assert result == "test string"
+
+"""
+Given a string with a capital at the beginning 
+and a '.' at the end #check will return True
+"""
+def test_check_returns_true_for_capital_at_beginning_and_full_stop_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("T.")
+    assert result == True
+
+"""
+Given a string with no capital at the beginning 
+and a '.' at the end #check will return False
+"""
+def test_check_returns_false_for_no_capital_at_beginning_and_full_stop_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("t.")
+    assert result == False
+
+"""
+Given a string with a capital at the beginning 
+and a no punctuation at the end #check will return False
+"""
+def test_check_returns_false_for_no_capital_at_beginning_and_no_punctuation_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("tp")
+    assert result == False
+
+"""
+Given a string with a capital at the beginning 
+and a ',' at the end #check will return True
+"""
+def test_check_returns_true_for_capital_at_beginning_and_comma_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("T:")
+    assert result == True
+
+"""
+Given a string with a capital at the beginning 
+and a '?' at the end #check will return True
+"""
+def test_check_returns_true_for_capital_at_beginning_and_question_mark_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("T?")
+    assert result == True
+
+"""
+Given a string with a capital at the beginning 
+and a '!' at the end #check will return True
+"""
+def test_check_returns_true_for_capital_at_beginning_and_exclamation_mark_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("T!")
+    assert result == True
+
+
+"""
+Given a string with a capital at the beginning 
+and a '!' at the end #check will return True
+"""
+def test_check_returns_true_for_capital_at_beginning_and_exclamation_mark_at_end():
+    test_instance = GrammarStats()
+    result = test_instance.check("T!")
+    assert result == True
+
+"""
+Given the #check method result is True
+#percentage_good will return 100
+"""
+def test_percentage_good_returns_100_when_check_is_true():
+    test_instance = GrammarStats() #create test instance
+    test_instance.check("T?") #pass #check a valid string
+    result = test_instance.percentage_good() #result is 100 with 1 valid string from #check
+    assert result == 100
+
+"""
+Given the #check method result is False
+#percentage_good will return 0
+"""
+def test_percentage_good_returns_0_when_check_is_flase():
+    test_instance = GrammarStats() #create test instance
+    test_instance.check("gdv") #pass #check a valid string
+    result = test_instance.percentage_good() #result is 0 with 0 valid string from #check
+    assert result == 0
+
+"""
+Given two #check methods results are True
+#percentage_good will return 100
+"""
+def test_percentage_good_returns_100_when_two_checks_are_True():
+    test_instance = GrammarStats() #create test instance
+    test_instance.check("W?") #pass #check a valid string
+    test_instance.check("T.") #pass #check another valid string
+    result = test_instance.percentage_good() #result is 0 with 0 valid string from #check
+    assert result == 100
